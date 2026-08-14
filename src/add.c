faa966b8ac7abaeacf89406ef54d49e6bd3147f7
Matthew James <matthewsalgorithm@gmail.com>
2026-08-14 21:11:38 +1000

    Added optional version specification via tag (as an extra argument to cute add

--- src/add.c
+++ src/add.c
@@ -9,12 +9,18 @@
 
 add_package_result_t add_package(int argc, char **argv) {
 	char* package_name = NULL;
+	char* package_version = NULL;
+
 	for (int i = 2; i < argc; i++) {
 		if (argv[i][0] != '-') {
-			if (package_name != NULL)
-				return ADD_INVALID_ARGS;
+			if (package_name == NULL) {
+				package_name = argv[i];
+			} else if (package_version == NULL) {
+				package_version = argv[i];
+			} else {
+			 	return ADD_INVALID_ARGS;
+			}
 
-			package_name = argv[i];
 			continue;
 		}
 	}
@@ -24,18 +30,21 @@ add_package_result_t add_package(int argc, char **argv) {
 
 	char* package_url = format("https://github.com/%s.git", package_name);
 
-	bool res = fetch_package(package_url, package_name, NULL);
+	bool res = fetch_package(package_url, package_name, package_version);
 
 	free(package_url);
 
-	if (!res) {
+	if (!res)
 		return ADD_FETCH_FAILED;
-	}
 
 	// TODO: properly locate root directory, we may be in a nested dir
 	ini_t* ini = ini_read("./Cute.ini");
 
-	ini_add_kv_pair(ini, "dependencies", package_name, "");
+	if (package_version == NULL) {
+		ini_add_kv_pair(ini, "dependencies", package_name, "");
+	} else {
+		ini_add_kv_pair(ini, "dependencies", package_name, package_version);
+	}
 
 	ini_write(ini);
 
